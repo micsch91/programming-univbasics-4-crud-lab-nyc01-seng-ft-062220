01a7f4c91257cbbd916ecef65873488d39cf66bd
Michael Schneider <mic.sch91@gmail.com>
2020-06-01 18:18:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,8 @@ add_element_to_end_of_array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-
+array = ["I", "am", "really", "learning"]
+array.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
